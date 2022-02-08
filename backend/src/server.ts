992f2b80826c98968c652c06de00deed056a1ff8
DanielGustavo <dg520589@gmail.com>
2022-02-08 18:08:42 -0300

    feat(backend): setup graphql-upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 import { startGraphQlServer } from './graphql';
 
@@ -6,6 +7,11 @@ const PORT = process.env?.PORT || 4001;
 
 async function startServer() {
   const app = express();
+
+  app.use(
+    '/graphql',
+    graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 5 })
+  );
 
   const graphqlServer = await startGraphQlServer();
   graphqlServer.applyMiddleware({ app, path: '/graphql' });
