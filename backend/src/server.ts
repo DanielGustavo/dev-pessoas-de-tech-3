7ec94834a3d525bb3a77fbbcf34d526c17ef2a53
DanielGustavo <dg520589@gmail.com>
2022-02-08 20:01:43 -0300

    feat(backend): add url to access static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { graphqlUploadExpress } from 'graphql-upload';
+import path from 'path';
 
 import { startGraphQlServer } from './graphql';
 
@@ -12,6 +13,8 @@ async function startServer() {
     '/graphql',
     graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 5 })
   );
+
+  app.use('/static', express.static(path.join(__dirname, '..', 'uploads')));
 
   const graphqlServer = await startGraphQlServer();
   graphqlServer.applyMiddleware({ app, path: '/graphql' });
